3c6fce43d6f847cd8f43c6c640602d55f9a350a7
avnerpnini <avnerp@outlook.com>
2016-07-19 10:23:26 +0300

    100% height problem in jqm - try to fix it

--- www/js/index.js
+++ www/js/index.js
@@ -120,6 +120,31 @@ function onDeviceReady(){
     if (QueryString["istest"]) {
         testQuestion(QueryString["quesID"]);
     }
+
+    //fix the 100% height bug taked from http://stackoverflow.com/questions/21552308/set-content-height-100-jquery-mobile/27617438#27617438
+//    var screen = $.mobile.getScreenHeight();
+//    var header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight();
+//    var footer = $(".ui-footer").hasClass("ui-footer-fixed") ? $(".ui-footer").outerHeight() - 1 : $(".ui-footer").outerHeight();
+    /* content div has padding of 1em = 16px (32px top+bottom). This step
+       can be skipped by subtracting 32px from content var directly. */
+//    var contentCurrent = $(".ui-content").outerHeight() - $(".ui-content").height();
+//    var content = screen - header - footer - contentCurrent;
+//    $(".ui-content").height(content);
+    //
+    
+    //fix the 100% height bug taked from http://stackoverflow.com/questions/21552308/set-content-height-100-jquery-mobile/27617438#27617438
+    function ScaleContentToDevice(){
+        scroll(0, 0);
+        var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
+        $(".ui-content").height(content);
+    }
+    $(document).on( "pagecontainershow", function(){
+        ScaleContentToDevice();        
+    });
+
+    $(window).on("resize orientationchange", function(){
+        ScaleContentToDevice();
+    });
  };
 
 //for Open panel on swipe
