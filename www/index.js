a0c973073b18f9fe0a2cd7bd6cc290efcc48bce9
Avner Pnini <avnerp@outlook.com>
2018-01-28 15:32:45 +0200

    y

--- www/index.js
+++ www/index.js
@@ -1,4 +1,5 @@
 
+//Example 1
 function success() {
     alert('BlueCats SDK is purring');
     //Start watching beacons using com.bluecats.beacons.watchEnterBeacon etc.
@@ -20,140 +21,107 @@ function startPuring (){
     com.bluecats.beacons.startPurringWithAppToken('00caffac-9ecd-42b9-8b75-1bd46a02f58c', success, error, sdkOptions);
     alert(2);
 }
-var blueCatsAppToken = 'BLUECATS-APP-TOKEN';
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('received');
-        app.watchBeacons();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(event) {
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+//Example 2
+function success2(watchData) {
+    var beacons = watchData.filteredMicroLocation.beacons;
+    var sites = watchData.filteredMicroLocation.sites;
+      alert('Beacons: ' + beacons.length + '\n' +
+            'Sites: ' + sites.length);
+  };
+  
+  function error2() {
+      alert('error2!');
+  };
+  
+  var beaconWatchOptions2 = {
+    minimumTriggerIntervalInSeconds:2, //Integer. Minimum seconds between callbacks (default 1)
+    repeatCount:3, //Integer. Default repeat infinite
+      filter:{
+        minimumProximity:'BC_PROXIMITY_IMMEDIATE', //String. Closest proximity to include. Default BC_PROXIMITY_IMMEDIATE
+        maximumProximity:'BC_PROXIMITY_NEAR', //String. Furthest proximity to include. Default BC_PROXIMITY_UNKNOWN
+        minimumAccuracy:0, //Number. Minimum distance in metres (Default 0)
+          maximumAccuracy:0.5, //Number. Maximum distance in metres (Default unrestricted)
+          sitesNamed:['Site1','Another Site'],//Array of string. Only include beacons in specified sites
+          categoriesNamed:['Entrance','Another Category'],//Array of string. Only include beacons in specified categories
+  
+      }
+  };
+  
+  var watchID = com.bluecats.beacons.watchMicroLocation(success2,error2, beaconWatchOptions2);
 
-        if(event == 'apptokenrequired'){
-            receivedElement.innerHTML = 'App token not set'
-        } else if(event == 'bluecatspurring'){
-            receivedElement.innerHTML = 'Looking for beacons'
-        };
 
-        console.log('Received Event: ' + event);
-    },
+  
+//Example 3
+  function success3(watchData) {
+    var beacons = watchData.filteredMicroLocation.beacons;
+    var sites = watchData.filteredMicroLocation.sites;
+      alert('Entered Beacons: ' + beacons.length + '\n' +
+            'Sites: ' + sites.length);
+  };
+  
+  function error3() {
+      alert('error3!');
+  };
+  
+  var beaconWatchOptions3 = {
+    minimumTriggerIntervalInSeconds:2, //Integer. Minimum seconds between callbacks (default 1)
+    repeatCount:3, //Integer. Default repeat infinite
+    secondsBeforeExitBeacon:5, //Integer. Seconds after beacon leaves range before it can re-enter (Default 5)
+      filter:{
+        minimumProximity:'BC_PROXIMITY_IMMEDIATE', //String. Closest proximity to include. Default BC_PROXIMITY_IMMEDIATE
+        maximumProximity:'BC_PROXIMITY_NEAR', //String. Furthest proximity to include. Default BC_PROXIMITY_UNKNOWN
+        minimumAccuracy:0, //Number. Minimum distance in metres (Default 0)
+          maximumAccuracy:10 //Number. Maximum distance in metres (Default unrestricted)
+      }
+  };
+  
+  var watchID = com.bluecats.beacons.watchEnterBeacon(success3,
+                                                         error3,
+                                                         beaconWatchOptions3);
 
-    watchBeacons: function(){
+//Example 5
+function success5(watchData) {
+    var closestBeacon = watchData.filteredMicroLocation.beacons[0];
+    var site = watchData.filteredMicroLocation.sites[0];
+      alert('Closest Beacon is: ' + closestBeacon.name + '\n' +
+            'In Site: ' + site.name);
+  };
+  
 
-        var watchIdForEnterBeacon,watchIdForExitBeacon,watchIdForClosestBeacon = null;
-        var beaconDisplayList = null;
+  
+  function error5() {
+      alert('error5!');
+  };
+  
+  var beaconWatchOptions5 = {
+    minimumTriggerIntervalInSeconds:2, //Integer. Minimum seconds between callbacks (default 1)
+    repeatCount:3, //Integer. Default repeat infinite
+    secondsBeforeExitBeacon:5, //Integer. Seconds after beacon leaves range before counts as 'exited' (Default 5)
+      filter:{
+        minimumProximity:'BC_PROXIMITY_IMMEDIATE', //String. Closest proximity to include. Default BC_PROXIMITY_IMMEDIATE
+        maximumProximity:'BC_PROXIMITY_UNKNOWN', //String. Furthest proximity to include. Default BC_PROXIMITY_UNKNOWN
+        minimumAccuracy:0, //Number. Minimum distance in metres (Default 0)
+          maximumAccuracy:10, //Number. Maximum distance in metres (Default unrestricted)
+          sitesNamed:[],//Array of string. Only include beacons in specified sites
+          categoriesNamed:[]//Array of string. Only include beacons in specified categories
+      }
+  };
+  
+  var watchID = com.bluecats.beacons.watchClosestBeaconChange(success5,
+                                                         error5,
+                                                         beaconWatchOptions5);
 
-        if(blueCatsAppToken == 'BLUECATS-APP-TOKEN'){
-            //BlueCats app token hasn't been configured
-            app.receivedEvent('apptokenrequired');
-            return;
-        }
 
-        var sdkOptions = {
-            useLocalStorage:true
-        };
 
-        var beaconWatchOptions = {
-            filter:{
-                //Configure additional filters here e.g.
-                //sitesName:['BlueCats HQ', 'Another Site'],
-                //categoriesNamed:['Entrance'],
-                //maximumAccuracy:0.5
-                //etc.
-            }
-        };
-
-        com.bluecats.beacons.startPurringWithAppToken(
-            blueCatsAppToken,
-            purringSuccess, logError, sdkOptions);
-
-        function purringSuccess() {
-            app.receivedEvent('bluecatspurring');
-            watchBeaconEntryAndExit();
-            watchClosestBeacon();
-        }
-
-        function watchBeaconEntryAndExit(){
-            if (watchIdForEnterBeacon != null) {
-                com.bluecats.beacons.clearWatch(watchIdForEnterBeacon);
-            };
-
-            if (watchIdForExitBeacon != null) {
-                com.bluecats.beacons.clearWatch(watchIdForExitBeacon);
-            };
-
-            watchIdForEnterBeacon = com.bluecats.beacons.watchEnterBeacon(
-                function(watchData){
-                    displayBeacons('Entered', watchData);
-                }, logError, beaconWatchOptions);
-            watchIdForExitBeacon = com.bluecats.beacons.watchExitBeacon(
-                function(watchData){
-                    displayBeacons('Exited', watchData);
-                }, logError, beaconWatchOptions);
-        }
-
-        function watchClosestBeacon(){
-            if (watchIdForClosestBeacon != null) {
-                com.bluecats.beacons.clearWatch(watchIdForClosestBeacon);
-            };
-
-            watchIdForClosestBeacon = com.bluecats.beacons.watchClosestBeaconChange(
-                function(watchData){
-                    displayBeacons('Closest to', watchData);
-                }, logError, beaconWatchOptions);
-        }
-
-        function displayBeacons(description, watchData){
-            var beacons = watchData.filteredMicroLocation.beacons;
-            var beaconNames = [];
-
-            for (var i = 0; i < beacons.length; i++) {
-                beaconNames.push(beacons[i].name);
-            };
-
-            var displayText = description + ' ' + beacons.length + ' beacons: ' + beaconNames.join(',');
-            console.log(displayText);
-
-            if(!beaconDisplayList){
-                var appElement = document.querySelector('.app');
-                beaconDisplayList = document.createElement('ol');
-                beaconDisplayList.setAttribute('id', 'beacons');
-                appElement.appendChild(beaconDisplayList);
-            }
-
-            var li = document.createElement('li');
-            li.appendChild(document.createTextNode(displayText));
-            beaconDisplayList.appendChild(li);
-        }
-
-        function logError() {
-            console.log('Error occurred watching beacons');
-        }
-    }
+//Example 7
+function success7(notificationData) {
+    alert('Notification received' + JSON.stringify(notificationData));
 };
 
-app.initialize();
+function error7() {
+    alert('error!');
+};
 
-
+com.bluecats.beacons.localNotificationReceived(success7, error7);
